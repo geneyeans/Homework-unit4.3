cf87843962ad178f9aaab47ca89f863b39489655
Benjarat Burapajetrak <85210845+geneyeans@users.noreply.github.com>
2022-03-11 09:05:51 +0700

    Add files via upload

---
+++ Homework5_1.java
@@ -0,0 +1,21 @@
+package homework5;
+
+public class Homework5_1 {
+public static void main(String[] args) {
+	int score = 70;
+	switch (score) {
+	case 80 :
+		System.out.println("grade A"); break;
+	case 70 :
+		System.out.println("grade B"); break;
+	case 60 :
+		System.out.println("grade C"); break;
+	case 50 :
+		System.out.println("grade D"); break;
+	case 40 :
+		System.out.println("grade F"); break;
+	default :
+		System.out.println("grade E"); break;
+	}
+}
+}
